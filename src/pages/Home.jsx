2f18ae4ee43a142037547b908878994dabbb7e08
MadhavaReddyyanamala <119478070+MadhavaReddyyanamala@users.noreply.github.com>
2025-03-23 13:43:54 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 align="center"><b>Welcome Home</b> </h1>
+      <img src="./immr.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Home
